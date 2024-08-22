cb5c9ba0af4e8317164a1b1cc9983599e2009b04
Compulink-Dev <devcompulink@gmail.com>
2024-08-22 14:38:40 +0200

    main

--- app/(front)/_components/testimonials.tsx
+++ app/(front)/_components/testimonials.tsx
@@ -19,7 +19,7 @@ function Testimonials() {
                             What Client Says
                         </h2>
                         <p className="text-gray-400 text-lg py-4">
-                            "Working with Compulink has been a game-changer for our business. Their deep expertise in hardware, software, and networking solutions ensured that we received a comprehensive and tailored approach to our tech needs. From upgrading our infrastructure to optimizing our software stack and streamlining our network, their team demonstrated exceptional knowledge and support at every step.
+                            Working with Compulink has been a game-changer for our business. Their deep expertise in hardware, software, and networking solutions ensured that we received a comprehensive and tailored approach to our tech needs. From upgrading our infrastructure to optimizing our software stack and streamlining our network, their team demonstrated exceptional knowledge and support at every step.
                         </p>
                         <p className="text-gray-400 text-lg pb-4">
                             Their commitment to excellence and innovation has significantly enhanced our operational efficiency and performance. I highly recommend <span className=""><Link href={'https://compulink.co.zw/'}>Compulink</Link></span> to anyone seeking reliable and cutting-edge tech solutions.
