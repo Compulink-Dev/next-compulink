458db81b3a36e3ecb82212937b6b47f14334da31
Compulink-Dev <devcompulink@gmail.com>
2024-09-23 12:15:38 +0200

    main

--- app/(front)/_components/testimonies.tsx
+++ app/(front)/_components/testimonies.tsx
@@ -39,7 +39,7 @@ export default function Testimonies() {
                                         <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
                                     </svg>
                                     <blockquote>
-                                        <p className="text-2xl italic font-medium text-gray-900 dark:text-white bg-gray-400 p-4 rounded-lg m-4">
+                                        <p className="italic font-medium text-gray-900 dark:text-white bg-gray-400 p-4 rounded-lg m-4">
                                             {testimony.description}
                                         </p>
                                     </blockquote>
