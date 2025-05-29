aae2e319ecd2fbcb992191a8796c997532a51784
Compulink-Dev <devcompulink@gmail.com>
2025-05-29 15:47:24 +0200

    homeLayout

--- components/homeLayout.tsx
+++ components/homeLayout.tsx
@@ -1,23 +1,21 @@
-import Footer from '@/app/(front)/_components/bottom'
-import Header from '@/app/(front)/_components/header'
-import Hero from '@/app/(front)/_components/hero'
-import HeroCarousel from '@/app/(front)/_components/heroCarousel'
+import Footer from "@/app/(front)/_components/bottom";
+import Hero from "@/app/(front)/_components/hero";
+import HeroCarousel from "@/app/(front)/_components/heroCarousel";
 
-import Navbar from '@/app/(front)/_components/navbar'
-import React from 'react'
+import Navbar from "@/app/(front)/_components/navbar";
+import React from "react";
 
 function HomeLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <div>
-            {/* <Navbar /> */}
-            <Header />
-            <main className="">
-                <HeroCarousel />
-                {children}
-            </main>
-            <Footer />
-        </div>
-    )
+  return (
+    <div>
+      {/* <Navbar /> */}
+      <main className="">
+        <HeroCarousel />
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-export default HomeLayout
+export default HomeLayout;
