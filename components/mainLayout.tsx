137cacc5834e59abd8192f55f6723cdd62d5a7ad
Compulink-Dev <devcompulink@gmail.com>
2025-05-29 15:12:00 +0200

    header

--- components/mainLayout.tsx
+++ components/mainLayout.tsx
@@ -1,22 +1,26 @@
-import Footer from '@/app/(front)/_components/bottom'
-import Header from '@/app/(front)/_components/header'
-import Hero from '@/app/(front)/_components/hero'
+import Footer from "@/app/(front)/_components/bottom";
+import Hero from "@/app/(front)/_components/hero";
 
-import Navbar from '@/app/(front)/_components/navbar'
-import React from 'react'
+import Navbar from "@/app/(front)/_components/navbar";
+import React from "react";
 
-function MainLayout({ children, backImage, image }: { children: React.ReactNode, backImage: any, image: any }) {
-    return (
-        <div>
-            {/* <Navbar /> */}
-            <Header />
-            <Hero backImage={backImage} image={image} />
-            <main className="">
-                {children}
-            </main>
-            <Footer />
-        </div>
-    )
+function MainLayout({
+  children,
+  backImage,
+  image,
+}: {
+  children: React.ReactNode;
+  backImage: any;
+  image: any;
+}) {
+  return (
+    <div>
+      {/* <Navbar /> */}
+      <Hero backImage={backImage} image={image} />
+      <main className="">{children}</main>
+      <Footer />
+    </div>
+  );
 }
 
-export default MainLayout
+export default MainLayout;
